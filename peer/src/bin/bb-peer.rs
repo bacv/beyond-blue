a6ac2e2ba20bdfdba38ca8ba7a710c94f5ff5533
gusto <bacv@users.noreply.github.com>
2022-08-17 00:24:09 +0300

    Swarmsvc (#1)
    * Extracted libp2p code to it's seperate modules in peer
    
    * Using channels to communicate via swarm svc

--- peer/src/bin/bb-peer.rs
+++ peer/src/bin/bb-peer.rs
@@ -1,44 +1,12 @@
-use async_std::io;
-use async_std::io::prelude::BufReadExt;
 use clap::Parser;
-use futures::executor::block_on;
-use futures::future::FutureExt;
-use futures::select;
-use futures::stream::StreamExt;
-use libp2p::core::multiaddr::{Multiaddr, Protocol};
-use libp2p::core::transport::OrTransport;
-use libp2p::core::upgrade;
-use libp2p::dns::DnsConfig;
-use libp2p::gossipsub::{
-    GossipsubEvent, GossipsubMessage, IdentTopic as Topic, MessageAuthenticity, MessageId,
-    ValidationMode,
-};
-use libp2p::identify::{Identify, IdentifyConfig, IdentifyEvent, IdentifyInfo};
-use libp2p::noise;
-use libp2p::ping::{Ping, PingConfig, PingEvent};
-use libp2p::relay::v2::client::{self, Client};
-use libp2p::swarm::{SwarmBuilder, SwarmEvent};
-use libp2p::tcp::{GenTcpConfig, TcpTransport};
-use libp2p::Transport;
-use libp2p::{dcutr, gossipsub};
-use libp2p::{identity, NetworkBehaviour, PeerId};
-use log::info;
-use peer::Event;
-use std::collections::hash_map::DefaultHasher;
-use std::convert::TryInto;
+use futures::StreamExt;
+use libp2p::{Multiaddr, PeerId};
 use std::error::Error;
-use std::hash::{Hash, Hasher};
-use std::net::Ipv4Addr;
-use std::str::FromStr;
-use std::time::Duration;
+use tokio::sync::mpsc;
 
 #[derive(Debug, Parser)]
-#[clap(name = "libp2p DCUtR client")]
+#[clap(name = "Example Beyond Blue peer")]
 struct Opts {
-    /// The mode (client-listen, client-dial).
-    #[clap(long)]
-    mode: Mode,
-
     /// Fixed value to generate deterministic peer id.
     #[clap(long)]
     secret_key_seed: u8,
@@ -58,229 +26,40 @@ enum Mode {
     Listen,
 }
 
-impl FromStr for Mode {
-    type Err = String;
-    fn from_str(mode: &str) -> Result<Self, Self::Err> {
-        match mode {
-            "dial" => Ok(Mode::Dial),
-            "listen" => Ok(Mode::Listen),
-            _ => Err("Expected either 'dial' or 'listen'".to_string()),
-        }
-    }
-}
-
-#[async_std::main]
+#[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
     let opts = Opts::parse();
 
-    let local_key = generate_ed25519(opts.secret_key_seed);
-    let local_peer_id = PeerId::from(local_key.public());
-    info!("Local peer id: {:?}", local_peer_id);
+    let id = peer::Identity::from_file("nothing".into());
+    let (local_in, local_out) = mpsc::channel(32);
+    let (remote_in, mut remote_out) = mpsc::channel(32);
 
-    let (relay_transport, client) = Client::new_transport_and_behaviour(local_peer_id);
-
-    let noise_keys = noise::Keypair::<noise::X25519Spec>::new()
-        .into_authentic(&local_key)
-        .expect("Signing libp2p-noise static DH keypair failed.");
-
-    let transport = OrTransport::new(
-        relay_transport,
-        block_on(DnsConfig::system(TcpTransport::new(
-            GenTcpConfig::default().port_reuse(true),
-        )))
-        .unwrap(),
-    )
-    .upgrade(upgrade::Version::V1)
-    .authenticate(noise::NoiseConfig::xx(noise_keys).into_authenticated())
-    .multiplex(libp2p_yamux::YamuxConfig::default())
-    .boxed();
-
-    let topic = Topic::new("test-net");
-    // To content-address message, we can take the hash of message and use it as an ID.
-    let message_id_fn = |message: &GossipsubMessage| {
-        let mut s = DefaultHasher::new();
-        message.data.hash(&mut s);
-        MessageId::from(s.finish().to_string())
+    let h1 = tokio::spawn(async move {
+        peer::SwarmSvc::new_with_default_transport(id.get_key())
+            .await
+            .spawn(
+                opts.relay_address,
+                opts.remote_peer_id,
+                remote_in,
+                local_out,
+            )
+            .await
+    });
+    let remote_stream = async_stream::stream! {
+        while let Some(item) = remote_out.recv().await {
+            yield item;
+        }
     };
+    let remote_stream = remote_stream.fuse();
 
-    // Set a custom gossipsub
-    let gossipsub_config = gossipsub::GossipsubConfigBuilder::default()
-        .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-        .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
-        .message_id_fn(message_id_fn) // content-address messages. No two messages of the
-        // same content will be propagated.
-        .build()
-        .expect("Valid config");
-    // build a gossipsub network behaviour
-    let mut gossipsub: gossipsub::Gossipsub = gossipsub::Gossipsub::new(
-        MessageAuthenticity::Signed(local_key.clone()),
-        gossipsub_config,
-    )
-    .expect("Correct configuration");
-
-    // subscribes to our topic
-    gossipsub.subscribe(&topic).unwrap();
-
-    let behaviour = peer::Behaviour {
-        relay_client: client,
-        ping: Ping::new(PingConfig::new().with_keep_alive(true)),
-        identify: Identify::new(IdentifyConfig::new(
-            "/TODO/0.0.1".to_string(),
-            local_key.public(),
-        )),
-        dcutr: dcutr::behaviour::Behaviour::new(),
-        gossip: gossipsub,
-    };
-
-    let mut swarm = SwarmBuilder::new(transport, behaviour, local_peer_id)
-        .dial_concurrency_factor(10_u8.try_into().unwrap())
-        .build();
-
-    swarm
-        .listen_on(
-            Multiaddr::empty()
-                .with("0.0.0.0".parse::<Ipv4Addr>().unwrap().into())
-                .with(Protocol::Tcp(0)),
-        )
-        .unwrap();
-
-    // Wait to listen on all interfaces.
-    block_on(async {
-        let mut delay = futures_timer::Delay::new(std::time::Duration::from_secs(1)).fuse();
-        loop {
-            futures::select! {
-                event = swarm.next() => {
-                    match event.unwrap() {
-                        SwarmEvent::NewListenAddr { address, .. } => {
-                            info!("Listening on {:?}", address);
-                        }
-                        event => panic!("{:?}", event),
-                    }
-                }
-                _ = delay => {
-                    // Likely listening on all interfaces now, thus continuing by breaking the loop.
-                    break;
-                }
-            }
+    tokio::pin!(remote_stream);
+    tokio::join!(h1, async {
+        while let msg = remote_stream.select_next_some().await {
+            log::info!("Msg: {}", msg);
         }
     });
 
-    // Connect to the relay server. Not for the reservation or relayed connection, but to (a) learn
-    // our local public address and (b) enable a freshly started relay to learn its public address.
-    swarm.dial(opts.relay_address.clone()).unwrap();
-    block_on(async {
-        let mut learned_observed_addr = false;
-        let mut told_relay_observed_addr = false;
-
-        loop {
-            match swarm.next().await.unwrap() {
-                SwarmEvent::NewListenAddr { .. } => {}
-                SwarmEvent::Dialing { .. } => {}
-                SwarmEvent::ConnectionEstablished { .. } => {}
-                SwarmEvent::Behaviour(Event::Ping(_)) => {}
-                SwarmEvent::Behaviour(Event::Identify(IdentifyEvent::Sent { .. })) => {
-                    info!("Told relay its public address.");
-                    told_relay_observed_addr = true;
-                }
-                SwarmEvent::Behaviour(Event::Identify(IdentifyEvent::Received {
-                    info: IdentifyInfo { observed_addr, .. },
-                    ..
-                })) => {
-                    info!("Relay told us our public address: {:?}", observed_addr);
-                    learned_observed_addr = true;
-                }
-                event => info!("{:?}", event),
-            }
-
-            if learned_observed_addr && told_relay_observed_addr {
-                break;
-            }
-        }
-    });
-
-    match opts.mode {
-        Mode::Dial => {
-            swarm
-                .dial(
-                    opts.relay_address
-                        .with(Protocol::P2pCircuit)
-                        .with(Protocol::P2p(opts.remote_peer_id.unwrap().into())),
-                )
-                .unwrap();
-        }
-        Mode::Listen => {
-            swarm
-                .listen_on(opts.relay_address.with(Protocol::P2pCircuit))
-                .unwrap();
-        }
-    }
-
-    let mut stdin = io::BufReader::new(io::stdin()).lines().fuse();
-
-    block_on(async {
-        loop {
-            select! {
-                line = stdin.select_next_some() => {
-                    if let Err(e) = swarm
-                        .behaviour_mut()
-                        .gossip
-                        .publish(topic.clone(), line.expect("Stdin not to close").as_bytes())
-                    {
-                        println!("Publish error: {:?}", e);
-                    }
-                },
-                event = swarm.select_next_some() => match event {
-                    SwarmEvent::NewListenAddr { address, .. } => {
-                        info!("Listening on {:?}", address);
-                    }
-                    SwarmEvent::Behaviour(Event::Relay(client::Event::ReservationReqAccepted {
-                        ..
-                    })) => {
-                        assert!(opts.mode == Mode::Listen);
-                        info!("Relay accepted our reservation request.");
-                    }
-                    SwarmEvent::Behaviour(Event::Relay(event)) => {
-                        info!("{:?}", event)
-                    }
-                    SwarmEvent::Behaviour(Event::Dcutr(event)) => {
-                        info!("{:?}", event)
-                    }
-                    SwarmEvent::Behaviour(Event::Identify(event)) => {
-                        info!("{:?}", event)
-                    }
-                    SwarmEvent::Behaviour(Event::Ping(_)) => {}
-                    SwarmEvent::Behaviour(Event::Gossipsub(GossipsubEvent::Message {
-                        propagation_source: peer_id,
-                        message_id: id,
-                        message,
-                    })) => println!(
-                        "Got message: {} with id: {} from peer: {:?}",
-                        String::from_utf8_lossy(&message.data),
-                        id,
-                        peer_id
-                    ),
-                    SwarmEvent::ConnectionEstablished {
-                        peer_id, endpoint, ..
-                    } => {
-                        info!("Established connection to {:?} via {:?}", peer_id, endpoint);
-                    }
-                    SwarmEvent::OutgoingConnectionError { peer_id, error } => {
-                        info!("Outgoing connection error to {:?}: {:?}", peer_id, error);
-                    }
-                    _ => {}
-                }
-            }
-        }
-    })
-}
-
-fn generate_ed25519(secret_key_seed: u8) -> identity::Keypair {
-    let mut bytes = [0u8; 32];
-    bytes[0] = secret_key_seed;
-
-    let secret_key = identity::ed25519::SecretKey::from_bytes(&mut bytes)
-        .expect("this returns `Err` only if the length is wrong; the length is correct; qed");
-    identity::Keypair::Ed25519(secret_key.into())
+    Ok(())
 }
